d8da7bd7341834b0032531de8166ed232e0c1847
Latte <cchukwuani@gmail.com>
2021-04-18 14:07:34 -0400

    restructured folders started server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3050
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONODB_URI || 'mongodb://localhost/fitnesslog', {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// Set Routes
+
+app.use(require('./routes/api.js'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
