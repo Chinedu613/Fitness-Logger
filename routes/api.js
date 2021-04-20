61f5e6e26552ab9fb03fd8618900e3261fd24003
Latte <cchukwuani@gmail.com>
2021-04-20 17:27:56 -0400

    pages are now linking to each other

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
-const Workout = require('../models/workout.js')
+const Workout = require('../models/workout.js');
+const path = require('path');
 
 
+
+router.get('/', async (req, res) => {
+    console.log("here")
+    try {
+        res.sendFile(path.join(`${__dirname}/index.html`));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/exercise'), async (req, res) => {
+    console.log('here!')
+    try {
+        res.sendFile(path.join(`${__dirname}/exercise.html`));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
 
 // Get all workouts 
 router.get('api/workouts', (req, res) => {
@@ -12,7 +31,7 @@ router.get('api/workouts', (req, res) => {
 });
 
 // Get workouts by ID
-router.get('api/workuts/:id', (req, res) => {
+router.get('api/workouts/:id', (req, res) => {
     
 
 });
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const PORT = process.env.PORT || 3050
 
@@ -7,9 +8,12 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('dev'))
 
+// Serving Static Files
 app.use(express.static('public'));
 
+// Connects Mongoose
 mongoose.connect(process.env.MONODB_URI || 'mongodb://localhost/workouts', {
     useNewUrlParser: true,
     useFindAndModify: false
@@ -18,7 +22,9 @@ mongoose.connect(process.env.MONODB_URI || 'mongodb://localhost/workouts', {
 // Set Routes
 
 app.use(require('./routes/api.js'));
+app.use(require('./routes/html.js'));
 
+// Running Port
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
 });
